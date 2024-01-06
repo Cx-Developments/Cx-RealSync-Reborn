3a9184527e0194057cba095046a45ceae9666704
Cx-Developments <71113137+Cx-Developments@users.noreply.github.com>
2024-01-07 02:44:04 +1100

    Update TimeWeatherConfiguration.lua

--- server/TimeWeatherConfiguration.lua
+++ server/TimeWeatherConfiguration.lua
@@ -1,4 +1,4 @@
-local TimeWeatherConfiguration = {
+TimeWeatherConfiguration = {
     ApiKey = "", -- https://www.weatherapi.com/signup.aspx generate an api key and place it here
     City = "Tasmania", -- EG (Australia, Tasmania)
     RefreshTime = 10 * 60000, -- (10 * 60000) 10 minutes default
