d4a408e87ca8af5f9e16efe0843e981cecccde54
Cx-Developments <71113137+Cx-Developments@users.noreply.github.com>
2024-01-07 02:46:22 +1100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Cx-Developments <https://github.com/Cx-Developments>'
-description 'Cx-RealSync 2.0 Rebuilt from the ground up '
+description 'Cx-RealSync 2.0 Rebuilt from the ground up'
 version '2.0'
 
 client_scripts {
